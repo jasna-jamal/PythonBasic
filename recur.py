b302d0fd95d9404ba294809aaa852949a444bcde
Jasna Jamal <jasnajamal2019@gmail.com>
2026-07-02 20:49:19 +0530

    Add files via upload

---
+++ recur.py
@@ -0,0 +1,26 @@
+#define a fn for countdown to 0 
+# def countdown(n):
+#     print(n)
+#     if  n==0:
+#         return 
+#     return countdown(n-1)
+# countdown(10)
+
+#Sum of 1st n numbers using recursion
+# number=int(input("Enter the Number:"))
+# def  findsum(n):
+#     if n==0:
+#         return 0
+#     return n+findsum(n-1)
+# print("Sum of 1st",number,"Natural Numbers:",end="")
+# print(findsum(number))
+
+
+#Factorial of  a  number
+# def fact(n):
+#     if n==1:
+#         return 1
+#     return n*fact(n-1)
+# print(fact(10))
+
+#BREAK, CONTINUE, PASS
