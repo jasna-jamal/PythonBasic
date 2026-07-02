c41294a98410d2fb72fa55fd073ebd7b18485378
Jasna Jamal <jasnajamal2019@gmail.com>
2026-07-02 12:02:12 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,19 @@
+a=int(input("Enter the first no:"))
+b=int(input("Enter the second number:"))
+
+summ=a+b
+diff=a-b
+product=a*b
+qutn=a/b
+floor = a//b
+mod=a%b
+print(summ)
+print(diff)
+print(product)
+print(qutn)
+print(floor)
+print(mod)
+s1=input("Enter first string:")
+s2=input("Enter second string:")
+new_string=s1+s2
+print(new_string)
